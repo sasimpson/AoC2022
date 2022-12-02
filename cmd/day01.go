0cd2e1c02a8823677f2af9c22891e43dd430ccdc
Scott Simpson <sasimpson@gmail.com>
2022-12-01 20:11:19 -0600

    day 1 solution

--- cmd/day01.go
+++ cmd/day01.go
@@ -4,8 +4,13 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"sort"
+	"strconv"
 )
 
 // day01Cmd represents the day01 command
@@ -18,7 +23,7 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: day01,
+	RunE: day01,
 }
 
 func init() {
@@ -35,6 +40,49 @@ func init() {
 	// day01Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-var day01 = func(cmd *cobra.Command, args []string) {
-	fmt.Println("day 01")
+var day01 = func(cmd *cobra.Command, args []string) error {
+	fmt.Println("day 1 part 1")
+
+	var foodList []int
+
+	file, err := os.Open("./data/day01-a.txt")
+	if err != nil {
+		return err
+	}
+	defer func() { _ = file.Close() }()
+
+	s := bufio.NewScanner(file)
+	var foodSum, maxFoodSum int
+	for s.Scan() {
+		line := s.Text()
+		if line == "" {
+			foodList = append(foodList, foodSum)
+			if foodSum > maxFoodSum {
+				maxFoodSum = foodSum
+			}
+			foodSum = 0
+			continue
+		}
+
+		foodCount, err := strconv.Atoi(line)
+		if err != nil {
+			return err
+		}
+
+		foodSum += foodCount
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("max calories:", maxFoodSum)
+
+	sort.Sort(sort.Reverse(sort.IntSlice(foodList)))
+	var maxSum int
+	for _, i := range foodList[:3] {
+		maxSum += i
+	}
+	fmt.Println("top three max:", maxSum)
+
+	return nil
 }
